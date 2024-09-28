ae2e22a5e62f95fc37c68d6f0a7c7871a2f71029
Mohamed Shaad <shaadclt@yahoo.com>
2024-09-28 12:25:12 +0530

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,116 @@
+import os
+import streamlit as st
+from streamlit_option_menu import option_menu
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
+from ml_utility import (read_data,
+                        preprocess_data,
+                        train_model,
+                        evaluate_model)
+
+
+# Get the working directory of the main.py file
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get the parent directory
+parent_dir = os.path.dirname(working_dir)
+
+st.set_page_config(
+    page_title="No-code ML Model Training",
+    page_icon="🧠",
+    layout="centered")
+
+
+st.title("No-code ML Model Training")
+
+dataset_list = os.listdir(f"{parent_dir}/data")
+
+dataset = st.selectbox("Select a dataset from the dropdown",
+                       dataset_list,
+                       index=None)
+
+df = read_data(dataset)
+
+if df is not None:
+    st.dataframe(df.head())
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    scaler_type_list = ["standard", "minmax"]
+
+    model_dictionary = {
+        "Logistic Regression": LogisticRegression(),
+        "Support Vector Classifier": SVC(),
+        "Random Forest Classifier": RandomForestClassifier(),
+        "XGBoost Classifier": XGBClassifier()
+    }
+
+
+    with col1:
+        target_column = st.selectbox("Select the Target Column", list(df.columns))
+    with col2:
+        scaler_type = st.selectbox("Select a scaler", scaler_type_list)
+    with col3:
+        selected_model = st.selectbox("Select a Model", list(model_dictionary.keys()))
+    with col4:
+        model_name = st.text_input("Model name")
+
+    if st.button("Train the Model"):
+
+        X_train, X_test, y_train, y_test = preprocess_data(df, target_column, scaler_type)
+
+        model_to_be_trained = model_dictionary[selected_model]
+
+        model = train_model(X_train, y_train, model_to_be_trained, model_name)
+
+        accuracy = evaluate_model(model, X_test, y_test)
+
+        st.success("Test Accuracy: " + str(accuracy))
+
+
+def set_bg_from_url(url, opacity=1):
+    
+    footer = """
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <footer>
+        <div style='visibility: visible;margin-top:7rem;justify-content:center;display:flex;'>
+            <p style="font-size:1.1rem;">
+                Made by Mohamed Shaad
+                &nbsp;
+                <a href="https://www.linkedin.com/in/mohamedshaad">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="white" class="bi bi-linkedin" viewBox="0 0 16 16">
+                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                    </svg>          
+                </a>
+                &nbsp;
+                <a href="https://github.com/shaadclt">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="white" class="bi bi-github" viewBox="0 0 16 16">
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                    </svg>
+                </a>
+            </p>
+        </div>
+    </footer>
+"""
+    st.markdown(footer, unsafe_allow_html=True)
+    
+    
+    # Set background image using HTML and CSS
+    st.markdown(
+        f"""
+        <style>
+            body {{
+                background: url('{url}') no-repeat center center fixed;
+                background-size: cover;
+                opacity: {opacity};
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+# Set background image from URL
+set_bg_from_url("https://www.fsm.ac.in/blog/wp-content/uploads/2022/08/ml-e1610553826718.jpg", opacity=0.875)
